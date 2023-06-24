9ef390c01f41cb3596980375dc8f50e7d6d396f8
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-24 17:41:08 -0300

    criação de componentes para a lista de capitais exibidas

--- src/components/capitais/capitais.tsx
+++ src/components/capitais/capitais.tsx
@@ -1,7 +1,13 @@
+import { ListaCapitais } from "./listacapitais";
+
 export function Capitais() {
   return (
     <div className="capitais">
-      <h2>Capitais</h2>
+      <h2 className="subtitle">Capitais</h2>
+      <div className="listaCapitais">
+        <ListaCapitais/>
+        <ListaCapitais/>
+      </div>
     </div>
   )
 }
---
+++ src/components/capitais/capital.tsx
@@ -0,0 +1,10 @@
+export function Capital() {
+  return (
+    <div className="capital">
+      <div className="tempMin">12°↓</div>
+      <div className="tempMax">25°↑</div>
+      <div className="cidade">exemplo city</div>
+    </div>
+
+  )
+}
