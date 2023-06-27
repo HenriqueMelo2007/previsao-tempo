9f116657ad6b709ec6a17fddceee723f16cedc26
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-27 18:24:24 -0300

    iniciando integração com a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { Previsao } from "./components/previsao/previsao"
 import { Pesquisar } from "./components/pesquisar/pesquisar"
 import { Capitais } from "./components/capitais/capitais"
+import { useState, useEffect } from "react"
+import axios from 'axios';
 
 export function App() {
 
-  console.log('olá')
+  const [nomeCidade, setNomeCidade] = useState('Campinas,SP')
+
+  useEffect(() => {
+    async function buscandoNaAPI() {
+      const urlAPI = `https://api.hgbrasil.com/weather?format=json&key=47984d0e&city_name=${nomeCidade}`
+      const jsonAPI = await axios.get(urlAPI)
+      console.log(jsonAPI)
+    }
+
+    buscandoNaAPI()
+  }, [])
 
   return (
     <div className="container">
       <div className="mini-container"><Previsao/></div>
-      <div className="mini-container"><Pesquisar/></div>
+      <div className="mini-container"> <Pesquisar nomeCidade={nomeCidade} setNomeCidade={setNomeCidade}></Pesquisar> </div>
       <div className="mini-container"><Capitais/></div>
     </div>
   )
--- src/components/pesquisar/pesquisar.tsx
+++ src/components/pesquisar/pesquisar.tsx
@@ -1,9 +1,15 @@
 import './pesquisar.css'
 
-export function Pesquisar() {
+type propsPesquisar = {
+  nomeCidade: string;
+  setNomeCidade: (value: string | ((prevState: string) => string)) => void;
+}
+
+export function Pesquisar({nomeCidade, setNomeCidade}: propsPesquisar) {
+
   return (
     <div className="pesquisar">
-      <input className="barraPesquisa" placeholder="Insira aqui o nome da cidade" type="text" />
+      <input onChange={value => setNomeCidade(value.target.value)} className="barraPesquisa" placeholder="Insira aqui o nome da cidade" type="text" />
     </div>
   )
 }
