b4c5aaed844d06ccf39445c6a608789101ca9c57
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-28 12:34:08 -0300

    integração com API feita

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import { Previsao } from "./components/previsao/previsao"
 import { Pesquisar } from "./components/pesquisar/pesquisar"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import axios from 'axios';
 
 export function App() {
 
   const [nomeCidade, setNomeCidade] = useState('')
 
-  useEffect(() => {
-    async function buscandoNaAPI() {
-      // const urlAPI = `9f74383adba6e40a4e2c23595a7265fa ${nomeCidade}`
-      const urlAPI = `http://api.openweathermap.org/data/2.5/weather?q=${nomeCidade}&appid=9f74383adba6e40a4e2c23595a7265fa`
-      const jsonAPI = await axios.get(urlAPI).then(e => e.data)
-      console.log(jsonAPI)
-    }
-
-    buscandoNaAPI()
-  }, [nomeCidade])
+  
+  async function buscandoNaAPI() {
+    const keyAPI = '9f74383adba6e40a4e2c23595a7265fa'
+      
+    const urlAPI = `http://api.openweathermap.org/data/2.5/weather?q=${nomeCidade}&appid=${keyAPI}&units=metric&lang=pt_br`
+    const jsonAPI = await axios.get(urlAPI).then(Response => Response.data)
+    console.log(jsonAPI)
+  }
 
   return (
     <div className="container">
-      <div className="mini-container"> <Pesquisar setNomeCidade={setNomeCidade}></Pesquisar> </div>
+      <div className="mini-container"> <Pesquisar setNomeCidade={setNomeCidade} buscandoNaAPI={buscandoNaAPI}></Pesquisar> </div>
       <div className="mini-container"><Previsao/></div>
-      
     </div>
   )
 }
--- src/components/pesquisar/pesquisar.tsx
+++ src/components/pesquisar/pesquisar.tsx
@@ -2,13 +2,22 @@ import './pesquisar.css'
 
 type propsPesquisar = {
   setNomeCidade: (value: string | ((prevState: string) => string)) => void;
+  buscandoNaAPI: () => Promise<void>;
 }
 
-export function Pesquisar({setNomeCidade}: propsPesquisar) {
+type enter = React.KeyboardEvent<HTMLInputElement>
+
+export function Pesquisar({setNomeCidade, buscandoNaAPI}: propsPesquisar) {
+
+  function buscar (event: enter) {
+    if (event.key == "Enter") {
+      buscandoNaAPI()
+    }
+  }
 
   return (
     <div className="pesquisar">
-      <input onChange={value => setNomeCidade(value.target.value)} className="barraPesquisa" placeholder="Insira aqui o nome da cidade" type="text" />
+      <input onKeyDown={buscar} onChange={value => setNomeCidade(value.target.value)} className="barraPesquisa" placeholder="Insira aqui o nome da cidade" type="text" />
     </div>
   )
 }
