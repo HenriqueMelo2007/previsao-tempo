df385c6a0a612fa70bda91329a92f3b97979c1d1
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-28 12:48:06 -0300

    adicionando informações

--- src/components/previsao/resultadoprevisao.tsx
+++ src/components/previsao/resultadoprevisao.tsx
@@ -1,14 +1,16 @@
 export function ResultadoPrevisao() {
   return (
       <div className="resultado">
-        <p>Cidade escolhida, EX - Brasil</p>
+        <p>Cidade escolhida, Brasil</p>
         <h2 className="tempEClima">20°C Chuvoso</h2>
 
         <div className="infoAdicionais">
-          <p>Min: 17°C</p>
-          <p>Máx: 25°C</p>
           <p>Sensação: <span>22°C</span></p>
+          <p>Min: <span>23°C</span></p>
+          <p>Máx: <span>25°C</span></p>
         </div>
+
+        <p className="description">Descrição: <span>Chuvoso</span></p>
 
       </div>
   )
